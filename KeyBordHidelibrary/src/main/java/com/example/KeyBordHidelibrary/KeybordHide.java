69a69fba2ce140c5824ec89e35c3f1840cc64549
13880201576 <13880201576>
2023-08-17 14:00:24 +0800

    安卓键盘

---
+++ KeyBordHidelibrary/src/main/java/com/example/KeyBordHidelibrary/KeybordHide.java
@@ -0,0 +1,27 @@
+package com.example.KeyBordHidelibrary;
+import android.app.Activity;
+import android.content.Context;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+public class KeybordHide {
+
+    public static void setup(Activity activity) {
+        View rootView = activity.getWindow().getDecorView().findViewById(android.R.id.content);
+        rootView.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                hideSoftKeyboard(activity);
+                return false;
+            }
+        });
+    }
+
+    private static void hideSoftKeyboard(Activity activity) {
+        View focusedView = activity.getCurrentFocus();
+        if (focusedView != null) {
+            InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(focusedView.getWindowToken(), 0);
+        }
+    }
+}
