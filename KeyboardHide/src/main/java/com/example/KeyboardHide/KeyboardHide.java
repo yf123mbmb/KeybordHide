561c3cbf0240d348eb6d6423ef96fd6d75318f10
小哥 <xiaoge@example.com>
2023-08-18 11:41:09 +0800

    2

---
+++ KeyboardHide/src/main/java/com/example/KeyboardHide/KeyboardHide.java
@@ -0,0 +1,31 @@
+package com.example.KeyboardHide;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+//com.keyboardhide.java
+
+public class KeyboardHide {
+
+    public static void setup(final Activity activity) {
+        View rootView = activity.getWindow().getDecorView().findViewById(android.R.id.content);
+        rootView.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                hideSoftKeyboard(activity);
+                return false;
+            }
+        });
+    }
+
+    private static void hideSoftKeyboard(Activity activity) {
+        View focusedView = activity.getCurrentFocus();
+        if (focusedView != null) {
+            InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(focusedView.getWindowToken(), 0);
+        }
+    }
+}
